351bf4d99d6c410e96bbe12feabba4dba954861f
sagarthapa07 <sagarthapa7757@gmail.com>
2025-08-22 13:03:58 +0530

    created seller sign up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { NgModel } from '@angular/forms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'seller-auth',
+        component:SellerAuthComponent
+    },
+        {
+        path:'',
+        component:HomeComponent
+    },
+];
