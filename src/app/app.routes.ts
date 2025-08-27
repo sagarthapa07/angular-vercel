a6305eec6ce85d6a914eb749f46695d417604b7f
sagarthapa07 <sagarthapa7757@gmail.com>
2025-08-27 16:30:05 +0530

    Add SellerAddProduct component and update routing
    - Created SellerAddProductComponent with associated HTML, CSS, and spec files.
    - Updated app.routes.ts to include route for SellerAddProductComponent.
    - Modified header to link to the new Add Product page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SellerAuthComponent } from './shared/components/seller-auth/seller-auth
 
 import { SellerHomeComponent } from './shared/components/seller-home/seller-home.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { SellerAddProductComponent } from './shared/components/seller-add-product/seller-add-product.component';
 
 
 export const routes: Routes = [
@@ -15,6 +16,11 @@ export const routes: Routes = [
     {
         path:'seller-home',
         component:SellerHomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path:'seller-add-product',
+        component:SellerAddProductComponent,
         canActivate: [AuthGuard]
     },
     
