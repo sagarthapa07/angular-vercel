6dfdf53692d589f60422fe7c4ee9695021c4a2a1
Sagar <sagarthapa7757@gmail.com>
2025-09-02 10:07:59 +0530

    Add SellerUpdateProduct component and update routing for product management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SellerAuthComponent } from './shared/components/seller-auth/seller-auth
 import { SellerHomeComponent } from './shared/components/seller-home/seller-home.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { SellerAddProductComponent } from './shared/components/seller-add-product/seller-add-product.component';
+import { SellerUpdateProductComponent } from './shared/components/seller-update-product/seller-update-product.component';
 
 
 export const routes: Routes = [
@@ -21,6 +22,11 @@ export const routes: Routes = [
     {
         path:'seller-add-product',
         component:SellerAddProductComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path:'seller-update-product',
+        component:SellerUpdateProductComponent,
         canActivate: [AuthGuard]
     },
     
--- src/app/shared/components/seller-home/seller-home.component.ts
+++ src/app/shared/components/seller-home/seller-home.component.ts
@@ -3,12 +3,13 @@ import { ProductsService } from '../../../core/sellerservice/products.service';
 import { Product } from '../../../dataType';
 import { NgForOf } from '@angular/common';
 import {FontAwesomeModule }from '@fortawesome/angular-fontawesome'
-import {faTrash } from '@fortawesome/free-solid-svg-icons';
+import {faTrash, faEdit} from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-seller-home',
-  imports: [NgForOf,FontAwesomeModule],
+  imports: [NgForOf, FontAwesomeModule, RouterLink],
   templateUrl: './seller-home.component.html',
   styleUrl: './seller-home.component.css'
 })
@@ -16,6 +17,7 @@ export class SellerHomeComponent {
   productList:undefined | Product[]
   productMessage:undefined |string;
   icon=faTrash;  
+  editIcon = faEdit
  constructor(private product:ProductsService){}
 
 
